2592210e01ae0a2c663d92b3a578eae47c351f5b
Fallet Steve <steve.fallet@divtec.ch>
2023-09-26 15:06:12 +0200

    ajout js

--- js/main.js
+++ js/main.js
@@ -4,5 +4,36 @@
  */
 
 'use strict';
+/********************************
+ CHANGEMENT ARRIERE-PLAN
+ ********************************/
 
-console.log('Test liaison JS');
+// Récupération du <body>
+const body = document.querySelector('body');
+const listeBg = document.querySelector('#bg');
+console.log(body, listeBg);
+// Change l'arrière-plan
+body.style.backgroundImage = `url(./img/backgrounds/${ listeBg.value })`;
+// Ecoute l'événement "change" sur la liste déroulante
+listeBg.addEventListener('change', function () {
+  body.style.backgroundImage = `url(./img/backgrounds/${ listeBg.value })`;
+});
+
+/********************************
+ ENVOI DU FORMULAIRE
+ ********************************/
+// Récupération du formulaire et des champs mot-de-passe
+const formulaire = document.querySelector('form');
+const txtPassword = document.querySelector('#pass');
+const txtPasswordConfirm = document.querySelector('#confirm');
+console.log(formulaire, txtPassword, txtPasswordConfirm);
+// Ecouter l'envoi du formulaire (submit)
+formulaire.addEventListener('submit', function () {
+  // Test si mot de passe et confirmation sont différents
+  if (txtPassword.value !== txtPasswordConfirm.value) {
+    alert('Mots de passes différents !');
+    return; // Termine la fonction
+  }
+
+  alert('Compte créé avec succès !')
+});
